b14f5c8e78df8a0db1f0e82378b92e59df9721ec
xander Hopkins <xanderhopkins@hotmail.com>
2017-11-15 20:52:24 -0500

    Added things
    Made the swich statements that i needed.

--- src/theVs/EnemyDealingDamage.java
+++ src/theVs/EnemyDealingDamage.java
@@ -2,9 +2,79 @@ package theVs;
 
 public class EnemyDealingDamage {
 
-	public static void main(String[] args) {
+	protected static void OpponentDealingDamageToYou(int which) {
 		// TODO Auto-generated method stub
+		switch (which){
+		case 1 :
+
+			if (){
+
+			}
+			else {
+				if () {
+
+				}
+				if () {
+
+				}
+			}
+			break;
+
+		case 2:
+			if (){
+
+			}
+			else {
+				if () {
+
+				}
+				if () {
+
+				}
+			}
+			break;
+		case 3:
+			if (){
+
+			}
+			else {
+				if () {
+
+				}
+				if () {
+
+				}
+
+			}
+			break;
+		case 4 :
+			if (){
+
+			}
+			else {
+				if () {
+
+				}
+				if () {
+
+				}
+			}
+			break;
+		case 5 :
+			if (){
+
+			}
+			else {
+				if () {
+
+				}
+				if () {
+
+				}
+			}
+			break;
+		}
+	}
 
 	}
 
-}
