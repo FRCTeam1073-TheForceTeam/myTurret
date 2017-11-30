6f7540e9bacc50b297f53094a54a17fc2eb7c013
xander Hopkins <xanderhopkins@hotmail.com>
2017-11-29 20:51:47 -0500

    Finished it.
    I have finally finish the game.

--- src/theVs/Battle.java
+++ src/theVs/Battle.java
@@ -1,5 +1,5 @@
 package theVs;
-import java.util.Scanner;
+import java.util.Scanner; 
 import java.util.ArrayList;
 import java.util.Random;
 //Author: Tristan Hoenninger
@@ -9,9 +9,9 @@ import java.util.Random;
 public class Battle extends DealingDamage {
 	static int OpponentBlasterHealth = 15;
 	static int OwnBlasterHealth = 15;
+	static int damage = 0;
 	final static int OwnHealth = 64;
 	final static int OpponentHealth = 64;
-	private String WhichPart;
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub 
 		ArrayList<String> EnemyChoice = new ArrayList<String>();
@@ -21,8 +21,7 @@ public class Battle extends DealingDamage {
 		EnemyChoice.add("Chest");
 		EnemyChoice.add("Blaster");
 		int OpponentTotalDamageTaken = 0;
-		int OwnTotalDamageTaken =0;
-		int damage = 0;
+		int OwnTotalDamageTaken = 0;
 		int Color = 0;
 		int YourColor = 0;
 		Random gen = new Random();
@@ -48,14 +47,23 @@ public class Battle extends DealingDamage {
 			if (WhichPartOfEnemy.equals("Blaster")) {
 				Color = 5;
 			}
-			DealingDamageToOpponent(Color);
-			System.out.println("You take aim at "+ WhichPartOfEnemy +".");
-			if( damage == 0) {
-				System.out.println("You fired but missed.");
+			damage = DealingDamageToOpponent(Color);
+			if (WhichPartOfEnemy.equals("Blaster")) {
+				OpponentBlasterHealth = OpponentBlasterHealth - damage;
+			}
+			if (OwnBlasterHealth <= 0) {
+				System.out.println("You throw a punch at the enemy's "+ WhichPartOfEnemy +".");
 			}
 			else {
-				System.out.println("You hit and dealt "+ damage +" damage.");
+				System.out.println("You take aim at "+ WhichPartOfEnemy +".");
+				if( damage == 0) {
+					System.out.println("You fired but missed.");
+				}
+				else {
+					System.out.println("You hit and dealt "+ damage +" damage.");
+				}
 			}
+			OwnTotalDamageTaken += damage; 
 			System.out.println( );
 			int BodyPartNum = gen.nextInt(5);
 			WhichPartOfYou = EnemyChoice.get(BodyPartNum);
@@ -74,13 +82,22 @@ public class Battle extends DealingDamage {
 			if (WhichPartOfYou.equals("Blaster")) {
 				YourColor = 5;
 			}
-			OpponentDealingDamageToYou(YourColor);
-			System.out.println("Opponent take aim at your "+ WhichPartOfYou +".");
-			if( damage == 0) {
-				System.out.println("Opponent fired but missed.");
+			damage = OpponentDealingDamageToYou(YourColor);
+			OpponentTotalDamageTaken += damage;
+			if (WhichPartOfYou.equals("Blaster")) {
+				OwnBlasterHealth = OwnBlasterHealth - damage;
+			}
+			if (OpponentBlasterHealth <= 0){
+				System.out.println("The opponent threw a punch at your "+ WhichPartOfYou +".");
 			}
 			else {
-				System.out.println("You were hit and took "+ damage +" damage.");
+				System.out.println("Opponent take aim at your "+ WhichPartOfYou +".");
+				if( damage == 0) {
+					System.out.println("Opponent fired but missed.");
+				}
+				else {
+					System.out.println("You were hit and took "+ damage +" damage.");
+				}
 			}
 		}
 		if (OwnTotalDamageTaken >= OwnHealth){
--- src/theVs/DealingDamage.java
+++ src/theVs/DealingDamage.java
@@ -1,79 +1,174 @@
 package theVs;
-
+import java.util.Random;
 
 public class DealingDamage {
 
-	protected static void DealingDamageToOpponent(int which){
+	protected static int DealingDamageToOpponent(int which){
 		// TODO Auto-generated method stub
+		int damage = 0;
+		Random gen = new Random();
 		switch (which){
+		//legs
 		case 1 :
 
-			if (){
-
+			if (Battle.OwnBlasterHealth <= 0){
+				damage = 4;
 			}
 			else {
-				if () {
-
+				int LegMissChance = gen.nextInt(8) + 1;
+				if (LegMissChance == 1) {
+					damage = 0;
 				}
-				if () {
-
+				if (LegMissChance != 1) {
+					damage = 7;
 				}
 			}
 			break;
-
+			//Head
 		case 2:
-			if (){
-
+			if (Battle.OwnBlasterHealth <= 0){
+				damage = 5;
 			}
 			else {
-				if () {
-
+				int HeadMissChance = gen.nextInt(5) + 1;
+				if (HeadMissChance == 1) {
+					damage = 0;
 				}
-				if () {
-
+				if (HeadMissChance != 1) {
+					damage = 10;
 				}
 			}
 			break;
+			//Arms
 		case 3:
-			if (){
-
+			if (Battle.OwnBlasterHealth <= 0){
+				damage = 3;
 			}
 			else {
-				if () {
-
+				int ArmMissChance = gen.nextInt(6) + 1;
+				if (ArmMissChance == 1) {
+					damage = 0;
 				}
-				if () {
-
+				if (ArmMissChance != 1) {
+					damage = 6;
 				}
-
 			}
+			//Chest
 			break;
 		case 4 :
-			if (){
-
+			if (Battle.OwnBlasterHealth <= 0){
+				damage = 2;
 			}
 			else {
-				if () {
-
+				int ChestMissChance = gen.nextInt(15) + 1;
+				if (ChestMissChance == 1) {
+					damage = 0;
 				}
-				if () {
-
+				if (ChestMissChance != 1) {
+					damage = 5;
 				}
 			}
 			break;
+			//Blaster
 		case 5 :
-			if (){
-
+			if (Battle.OwnBlasterHealth <= 0){
+				damage = 1;
 			}
 			else {
-				if () {
-
+				int BlasterMissChance = gen.nextInt(2) + 1;
+				if (BlasterMissChance == 1) {
+					damage = 0;
 				}
-				if () {
-
+				if (BlasterMissChance != 1) {
+					damage = 4;
 				}
 			}
 			break;
 		}
+		return damage;
 	}
+	protected static int OpponentDealingDamageToYou(int which) {
+		// TODO Auto-generated method stub
+		int damage = 0;
+		Random gen = new Random();
+		switch (which){
+		//Legs
+		case 1 :
+			if (Battle.OpponentBlasterHealth <= 0){
+				damage = 4;
+			}
+			else {
+				int LegMissChance = gen.nextInt(8) + 1;
+				if (LegMissChance == 1) {
+					Battle.damage = 0;
+				}
+				if (LegMissChance != 1) {
+					Battle.damage = 7;
+				}
+			}
+			break;
+			//Head
+		case 2:
+			if (Battle.OpponentBlasterHealth > 0){
+				damage = 5;
+			}
+			else {
+				int HeadMissChance = gen.nextInt(5) + 1;
+				if (HeadMissChance == 1) {
+					damage = 0;
+				}
+				if (HeadMissChance != 1) {
+					damage = 10;
+				}
+			}
+			break;
+			//Arms
+		case 3:
+			if (Battle.OpponentBlasterHealth <= 0){
+				damage = 3;
+			}
+			else {
+				int ArmMissChance = gen.nextInt(6) + 1;
+				if (ArmMissChance == 1) {
+					damage = 0;
+				}
+				if (ArmMissChance != 1) {
+					damage = 6;
+				}
+			}
+			break;
+			//Chest
+		case 4 :
+			if (Battle.OpponentBlasterHealth <= 0){
+				damage = 2;
+			}
+			else {
+				int ChestMissChance = gen.nextInt(15) + 1;
+				if (ChestMissChance == 1) {
+					damage = 0;
+				}
+				if (ChestMissChance != 1) {
+					damage = 5;
+				}
+			}
+			break;
+			//Blaster
+		case 5 :
+			if (Battle.OpponentBlasterHealth <= 0){
+				damage = 1;
+			}
+			else {
+				int BlasterMissChance = gen.nextInt(2) + 1;
+				if (BlasterMissChance == 1) {
+					damage = 0;
+				}
+				if (BlasterMissChance != 1) {
+					damage = 4;
+				}
+			}
+			break;
+		}
+		return damage;
+	}
+
 }
